06fe6e1e78184b16b8dca67f1532d6ab709fc47b
Ram Basnet <rambasnet@gmail.com>
2023-10-03 20:50:27 +0000

    complete issue #1

---
+++ hello/hello.py
@@ -0,0 +1,15 @@
+"""
+"""
+
+
+def answer() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    return 'Hello World!'
+
+
+if __name__ == "__main__":
+    print(answer())
---
+++ hello/test_hello.py
@@ -0,0 +1,10 @@
+"""
+"""
+
+import hello
+
+
+def test_answer() -> None:
+    """_summary_
+    """
+    assert hello.answer() == 'Hello World!'
