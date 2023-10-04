3a67375cf44e98b84805d5ed4a254ef1b6df66f5
Ram Basnet <rambasnet@gmail.com>
2023-10-04 16:27:31 +0000

    test add function

--- hello/hello.py
+++ hello/hello.py
@@ -11,5 +11,11 @@ def answer() -> str:
     return 'Hello World!'
 
 
+def add(num1: int, num2: int) -> int:
+    """
+    """
+    return num1 + num2
+
+
 if __name__ == "__main__":
     print(answer())
--- hello/test_hello.py
+++ hello/test_hello.py
@@ -8,3 +8,9 @@ def test_answer() -> None:
     """_summary_
     """
     assert hello.answer() == 'Hello World!'
+
+
+def test_add() -> None:
+    """
+    """
+    assert hello.add(1, 2) == 3
